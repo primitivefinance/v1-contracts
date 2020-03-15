120880ef428a046c46eb323955569c5668bff3e7
Alexangelj <alexangel.j@gmail.com>
2020-03-15 15:57:47 -0700

    2.7 altered deployment and fixed truffle dependenc

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,13 +6,13 @@ const tETH = artifacts.require('tETH');
 const tUSD = artifacts.require('tUSD')
 
 const supply = (10**20).toString();
-const million = (10**24).toString();
+const million = (10**20).toString();
 
 module.exports = async (deployer,accounts) => {
   deployer.deploy(Underlying, "Underlying", "U", 18, supply);
   deployer.deploy(Strike, "Strike", "S", 18, supply);
   deployer.deploy(Prime, "Prime Contract", "SP");
   deployer.deploy(Slate, "Slate NFT of Prime", "SP");
-  deployer.deploy(tEth, "Testnet ETH", 'tETH', 18, million);
+  deployer.deploy(tETH, "Testnet ETH", 'tETH', 18, million);
   deployer.deploy(tUSD, "Testnet USD", 'tUSD', 18, million);
 };
--- migrations/3_deploy_testcoins.js
+++
@@ -1,9 +0,0 @@
-const tETH = artifacts.require('tETH');
-const tUSD = artifacts.require('tUSD')
-
-let million = (10**20).toString();
-
-module.exports = async (deployer,accounts) => {
-  deployer.deploy(tETH, "Testnet ETH", 'tETH', 18, million);
-  deployer.deploy(tUSD, "Testnet USD", 'tUSD', 18, million);
-};
