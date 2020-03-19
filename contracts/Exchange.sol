c20c43875398871d558aa608b58ee11eeb88a63f
Alexangelj <alexangel.j@gmail.com>
2020-03-19 10:01:17 -0700

    3.05 exchange.sol

---
+++ contracts/Exchange.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.6.2;
+
+/**
+ * @title   DFCP's Exchange Contract
+ * @notice  A Decentralized Exchange to manage the buying and selling
+            of Prime Option tokens.
+ * @author  Decentralized Financial Crafting Protocol
+ */
+
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+
+contract Exchange {
+    using SafeMath for uint256;
+
+    
+}
