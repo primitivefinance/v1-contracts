e6d7302f8fa7617c209130dffabfb44949ab73f1
Alexangelj <alexangel.j@gmail.com>
2020-05-03 17:43:07 -0700

    added oracle test suite for premium

--- test/oracle.js
+++ test/oracle.js
@@ -1,42 +1,17 @@
-const { expect } = require("chai");
+const { assert, expect } = require("chai");
+const { BigNumber } = require("bignumber.js");
 const PrimeOracle = artifacts.require("PrimeOracle");
 
 contract("Oracle contract", accounts => {
     // WEB3
     const { toWei } = web3.utils;
     const { fromWei } = web3.utils;
-    const { getBalance } = web3.eth;
-
-    // ERROR CODES
-    const ERR_ZERO = "ERR_ZERO";
-    const ERR_BAL_ETH = "ERR_BAL_ETH";
-    const ERR_NOT_OWNER = "ERR_NOT_OWNER";
-    const ERR_BAL_PRIME = "ERR_BAL_PRIME";
-    const ERR_BAL_STRIKE = "ERR_BAL_STRIKE";
-    const ERR_BAL_REDEEM = "ERR_BAL_REDEEM";
-    const ERR_BAL_TOKENS = "ERR_BAL_TOKENS";
-    const ERR_BAL_OPTIONS = "ERR_BAL_OPTIONS";
-    const ERR_OPTION_TYPE = "ERR_OPTION_TYPE";
-
-    // COMMON AMOUNTS
-    const ROUNDING_ERR = 10**8;
-    const ONE_ETHER = toWei('1');
-    const TWO_ETHER = toWei('2');
-    const FIVE_ETHER = toWei('5');
-    const TEN_ETHER = toWei('10');
-    const FIFTY_ETHER = toWei('50');
-    const HUNDRED_ETHER = toWei('100');
-    const THOUSAND_ETHER = toWei('1000');
-    const MILLION_ETHER = toWei('1000000');
 
     // CORE ADDRESSES
     const MAINNET_DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
     const MAINNET_ORACLE = '0xdA17fbEdA95222f331Cb1D252401F4b44F49f7A0';
     const MAINNET_COMPOUND_DAI = '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643';
 
-    // ACCOUNTS
-    const Alice = accounts[0]
-    const Bob = accounts[1]
 
     let oracle;
 
@@ -51,7 +26,7 @@ contract("Oracle contract", accounts => {
         });
     });
 
-    describe("Calculation", () => {
+    describe("Calculation General", () => {
         it("Calculates the premium for ETH 200 DAI Put Expiring May 29", async () => {
             let deribit = '0.0765'; // in ethers
             let tokenU = MAINNET_DAI;
@@ -63,6 +38,134 @@ contract("Oracle contract", accounts => {
             console.log('[PREMIUM IN WEI]', (premium).toString());
             console.log('[PREMIUM]', fromWei(premium));
             console.log('[DERIBIT PREMIUM]', deribit);
+        });
+
+        it("Calculates premiums for arbritary options", async () => {
+
+            const calculatePremiums = async (volatility, base, price, expiry) => {
+                let tokenU = MAINNET_DAI;
+                let premium = await oracle.calculatePremium(tokenU, volatility, base, price, expiry);
+                return premium;
+            }
+
+            const generateRandomOption = () => {
+                let ethPrice = 200;
+                function randomDate(start, end) {
+                    let date = Math.floor(+start + Math.random() * (end - start));
+                    return date;
+                }
+                function getRandomArbitrary(min, max) {
+                    return Math.random() * (max - min) + min;
+                }
+                let option = {
+                    volatility: Math.floor(1000 * Math.random()),
+                    base: toWei((ethPrice * getRandomArbitrary(0.9, 1.1)).toString()),
+                    price: toWei('1'),
+                    expiry: randomDate(1588636800, 1590796740)
+                }
+
+                return option;
+            }
+
+            const run = async (amount) => {
+                for(let i = 0; i < amount; i++) {
+                    let option = generateRandomOption();
+                    let premium = await calculatePremiums(
+                        option.volatility,
+                        option.base,
+                        option.price,
+                        option.expiry
+                    );
+
+                    let calculation = {
+                        volatility: option.volatility,
+                        base: fromWei(option.base),
+                        price: fromWei(option.price),
+                        expiry: (new Date(option.expiry * 1000)).toDateString(),
+                        premium: fromWei(premium)
+                    }
+                    console.log(calculation);
+                }
+            }
+            
+            await run(1);
+        });
+    });
+
+    describe("Calculation Intrinsic", () => {
+
+        it("Calculates intrinsic premiums for arbritary options and compares to market", async () => {
+
+            const calculateIntrinsic = async (base, price) => {
+                let tokenU = MAINNET_DAI;
+                let intrinsic = await oracle.calculateIntrinsic(tokenU, base, price);
+                return intrinsic;
+            }
+
+            const generateRandomOption = () => {
+                let ethPrice = 200;
+                function randomDate(start, end) {
+                    let date = Math.floor(+start + Math.random() * (end - start));
+                    return date;
+                }
+                function getRandomArbitrary(min, max) {
+                    return Math.random() * (max - min) + min;
+                }
+                let option = {
+                    volatility: Math.floor(1000 * Math.random()),
+                    base: toWei((ethPrice * getRandomArbitrary(0.9, 1.1)).toString()),
+                    price: toWei('1'),
+                    expiry: randomDate(1588636800, 1590796740)
+                }
+
+                return option;
+            }
+
+            const run = async (amount) => {
+                for(let i = 0; i < amount; i++) {
+                    let option = generateRandomOption();
+                    let intrinsic = await calculateIntrinsic(
+                        option.base,
+                        option.price
+                    );
+                    let market = new BigNumber(await oracle.marketPrice(MAINNET_DAI));
+                    market = new BigNumber(10**36)
+                                    .dividedBy(market);
+                    // For puts
+                    let expected = 
+                                new BigNumber(option.base)
+                                    .minus(market);
+
+                    let difference;
+                    if(intrinsic >= expected) {
+                        difference = new BigNumber(intrinsic).minus(expected);
+                    } else {
+                        difference = 0;
+                    };
+
+                    let error = difference.dividedBy(new BigNumber(option.base)).multipliedBy(100);
+
+                    let calculation = {
+                        market: (market).toString(),
+                        base: option.base,
+                        price: option.price,
+                        intrinsic: (intrinsic).toString(),
+                        expected: (expected).toString(),
+                        difference: fromWei((Math.floor(difference)).toString()),
+                        error: (error).toString()
+                    }
+
+                    console.log(calculation);
+                    let max_error = 1.5; // %
+                    if(+intrinsic > 0) {
+                        assert.isAtMost(+error, +max_error);
+                    }
+                    
+                    
+                }
+            }
+            
+            await run(10);
         });
     });
 });
