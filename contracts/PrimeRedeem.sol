f520fbc6a292141c5f8631b69b4eb17e1cc8b368
Alexangelj <alexangel.j@gmail.com>
2020-05-20 13:26:15 -0700

    primeredeem will take fee if the feeReceiver address is set

--- contracts/PrimeRedeem.sol
+++ contracts/PrimeRedeem.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.2;
 
 /**
- * @title   Primitive's Redeem ERC-20 for Prime Options
+ * @title   Strike Redeem ERC-20 for Prime Vanilla Options
  * @author  Primitive
  */
 
@@ -10,43 +10,39 @@ import "./interfaces/IPrimitiveFactory.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-
 contract PrimeRedeem is IPrimeRedeem, ERC20 {
     using SafeMath for uint;
 
-    string public constant NAME = "Primitive Strike Redeem";
-    string public constant SYMBOL = "REDEEM";
-
-    uint public fee;
+    uint public constant FEE = 500;
+    string private constant NAME = "Primitive Strike Redeem";
+    string private constant SYMBOL = "REDEEM";
     address public override factory;
     address public override tokenS;
     address public override tokenP;
 
-
-    constructor (
-        address _tokenP,
-        address _tokenS
-    )
-        public
-        ERC20(NAME, SYMBOL)
-    {
+    constructor (address _tokenP, address _tokenS) public ERC20(NAME, SYMBOL) {
         factory = msg.sender;
         tokenS = _tokenS;
         tokenP = _tokenP;
     }
 
-    function mint(address to, uint amount) external override returns(bool)  {
+    function mint(address to, uint amount) external override {
         require(msg.sender == tokenP, "ERR_NOT_VALID");
         address feeReceiver = IPrimitiveFactory(factory).feeReceiver();
-        uint _fee = amount.div(fee);
-        _mint(to, amount.sub(_fee));
-        _mint(feeReceiver, _fee);
-        return true;
+        bool takeFee = feeReceiver != address(0);
+        if(!takeFee) {
+            _mint(to, amount);
+        } else return mintWithFee(to, amount);
     }
 
-    function burn(address to, uint amount) external override returns(bool)  {
+    function burn(address to, uint amount) external override {
         require(msg.sender == tokenP, "ERR_NOT_VALID");
         _burn(to, amount);
-        return true;
+    }
+
+    function mintWithFee(address to, uint amount) private {
+        uint _fee = amount.div(FEE);
+        _mint(to, amount.sub(_fee));
+        _mint(feeReceiver, _fee);
     }
 }
--- contracts/interfaces/IPrimeRedeem.sol
+++ contracts/interfaces/IPrimeRedeem.sol
@@ -4,6 +4,6 @@ interface IPrimeRedeem {
     function tokenS() external view returns (address);
     function tokenP() external view returns (address);
     function factory() external view returns (address);
-    function mint(address user, uint256 amount) external returns (bool);
-    function burn(address user, uint256 amount) external returns (bool);
+    function mint(address user, uint256 amount) external;
+    function burn(address user, uint256 amount) external;
 }
