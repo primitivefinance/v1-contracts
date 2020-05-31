018a48ad1d7fdf33a19d9ec737856d9e06d4085e
Alexangelj <alexangel.j@gmail.com>
2020-05-31 13:37:47 -0700

    Removed verifyBalance and redundant balance checks from Trader

--- contracts/extensions/PrimeTrader.sol
+++ contracts/extensions/PrimeTrader.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.6.2;
 
 /**
- * @title   Primitive's Trader Contract
- * @notice  Safely interacts with the Prime Vanilla Option Primitive
+ * @title   Trader
+ * @notice  Abstracts the interfacing with option contracts for ease-of-use.
  * @author  Primitive
  */
 
@@ -22,30 +22,23 @@ contract PrimeTrader is IPrimeTrader, ReentrancyGuard {
     event Redeem(address indexed from, uint256 inTokenR);
     event Close(address indexed from, uint256 inTokenP);
 
-    constructor (address payable _weth) public {
-        weth = _weth;
-    }
+    constructor (address payable _weth) public { weth = _weth; }
 
     /**
      * @dev Mint Primes by depositing tokenU.
      * @notice Also mints Prime Redeem tokens. Calls msg.sender with transferFrom.
-     * @param tokenP The address of the Prime Option to trade with.
+     * @param tokenP The address of the Prime Option contract.
      * @param amount Quantity of Prime options to mint and tokenU to deposit.
      * @param receiver The newly minted tokens are sent to the receiver address.
      */
     function safeMint(IPrime tokenP, uint256 amount, address receiver)
         external
-        nonReentrant
         override
+        nonReentrant
         returns (uint256 inTokenU, uint256 outTokenR)
     {
         require(amount > 0, "ERR_ZERO");
         address tokenU = tokenP.tokenU();
-        verifyBalance(
-            IERC20(tokenU).balanceOf(msg.sender),
-            amount,
-            "ERR_BAL_UNDERLYING"
-        );
         require(
             IERC20(tokenU).transferFrom(msg.sender, address(tokenP), amount),
             "ERR_TRANSFER_IN_FAIL"
@@ -71,18 +64,8 @@ contract PrimeTrader is IPrimeTrader, ReentrancyGuard {
     {
         require(amount > 0, "ERR_ZERO");
         address tokenS = tokenP.tokenS();
-        verifyBalance(
-            IERC20(address(tokenP)).balanceOf(msg.sender),
-            amount,
-            "ERR_BAL_PRIME"
-        );
 
         inTokenS = amount.mul(tokenP.price()).div(tokenP.base());
-        verifyBalance(
-            IERC20(tokenS).balanceOf(msg.sender),
-            inTokenS,
-            "ERR_BAL_STRIKE"
-        );
         (bool inTransferS) = IERC20(tokenS).transferFrom(msg.sender, address(tokenP), inTokenS);
         (bool inTransferP) = IERC20(address(tokenP)).transferFrom(msg.sender, address(tokenP), amount);
         require(inTransferS && inTransferP, "ERR_TRANSFER_IN_FAIL");
@@ -107,20 +90,9 @@ contract PrimeTrader is IPrimeTrader, ReentrancyGuard {
         address tokenS = tokenP.tokenS();
         address tokenR = tokenP.tokenR();
 
-        verifyBalance(
-            IERC20(tokenR).balanceOf(msg.sender),
-            amount,
-            "ERR_BAL_REDEEM"
-        );
-
         // There can be the case there is no available tokenS to redeem.
         // This is the first verification of a tokenS balance to draw from.
         // There is a second verification in the redeem() function.
-        verifyBalance(
-            IERC20(tokenS).balanceOf(address(tokenP)),
-            amount,
-            "ERR_BAL_STRIKE"
-        );
         IERC20(tokenR).transferFrom(msg.sender, address(tokenP), amount);
         (inTokenR) = tokenP.redeem(receiver);
         emit Redeem(msg.sender, inTokenR);
@@ -146,18 +118,6 @@ contract PrimeTrader is IPrimeTrader, ReentrancyGuard {
         address tokenR = tokenP.tokenR();
 
         inTokenR = amount.mul(tokenP.price()).div(tokenP.base());
-
-        verifyBalance(
-            IERC20(tokenR).balanceOf(msg.sender),
-            inTokenR,
-            "ERR_BAL_REDEEM"
-        );
-
-        verifyBalance(
-            IERC20(address(tokenP)).balanceOf(msg.sender),
-            amount,
-            "ERR_BAL_PRIME"
-        );
 
         IERC20(tokenR).transferFrom(msg.sender, address(tokenP), inTokenR);
         IERC20(address(tokenP)).transferFrom(msg.sender, address(tokenP), amount);
@@ -186,24 +146,8 @@ contract PrimeTrader is IPrimeTrader, ReentrancyGuard {
 
         inTokenR = amount.mul(tokenP.price()).div(tokenP.base());
 
-        verifyBalance(
-            IERC20(tokenR).balanceOf(msg.sender),
-            inTokenR,
-            "ERR_BAL_REDEEM"
-        );
-
         IERC20(tokenR).transferFrom(msg.sender, address(tokenP), inTokenR);
         (inTokenR, inTokenP, outTokenU) = tokenP.close(receiver);
         emit Close(msg.sender, inTokenP);
-    }
-
-    function verifyBalance(
-        uint256 balance,
-        uint256 minBalance,
-        string memory errorCode
-    ) internal pure {
-        minBalance == 0 ?
-            require(balance > minBalance, errorCode) :
-            require(balance >= minBalance, errorCode);
     }
 }
