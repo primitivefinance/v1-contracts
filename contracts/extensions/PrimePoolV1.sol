856508e9dd5a3d4513ffbd56e6359912a443d209
Alexangelj <alexangel.j@gmail.com>
2020-05-31 15:56:55 -0700

    Add internal functions to PoolV1 to interact with option, including flash

--- contracts/extensions/PrimePoolV1.sol
+++ contracts/extensions/PrimePoolV1.sol
@@ -47,12 +47,12 @@ contract PrimePoolV1 is IPrimePool, Ownable, Pausable, ReentrancyGuard, ERC20 {
 
         // If liquidity is not intiialized, mint the initial liquidity.
         if(_totalSupply == 0) {
-            outTokenPULP = inTokenU.mul(IPrime(_tokenP).price()).div(1 ether);
+            outTokenPULP = inTokenU;
         } else {
             outTokenPULP = inTokenU.mul(_totalSupply).div(totalBalance);
         }
 
-        require(outTokenPULP > 0, "ERR_ZERO_LIQUIDITY");
+        require(outTokenPULP > uint(0) && outTokenPULP >= MIN_LIQUIDITY, "ERR_LIQUIDITY");
         _mint(to, outTokenPULP);
         emit Deposit(to, inTokenU, outTokenPULP);
     }
@@ -61,19 +61,69 @@ contract PrimePoolV1 is IPrimePool, Ownable, Pausable, ReentrancyGuard, ERC20 {
         internal
         returns (uint outTokenU)
     {
-        // Check tokenPULP balance.
         require(balanceOf(to) >= inTokenPULP && inTokenPULP > 0, "ERR_BAL_PULP");
-        
-        // Store for gas savings.
         uint _totalSupply = totalSupply();
 
         // Calculate output amounts.
         outTokenU = inTokenPULP.mul(totalBalance).div(_totalSupply);
-        require(outTokenU > 0, "ERR_ZERO");
+        require(outTokenU > uint(0), "ERR_ZERO");
         // Burn tokenPULP.
         _burn(to, inTokenPULP);
-        // Push outTokenU to `to` address.
         emit Withdraw(to, inTokenPULP, outTokenU);
+    }
+
+    function _mint(address receiver, uint outTokenU) internal returns (uint outTokenP) {
+        address _tokenP = tokenP;
+
+        // Transfer underlying tokens to option contract.
+        IERC20(IPrime(_tokenP).tokenU()).transfer(_tokenP, outTokenU);
+
+        // Mint Prime and Prime Redeem to the receiver.
+        (outTokenP, ) = IPrime(_tokenP).mint(receiver);
+    }
+
+    function _exercise(address receiver, uint inTokenS, uint inTokenP)
+        internal
+        returns (uint outTokenU)
+    {
+        address _tokenP = tokenP;
+        // Transfer strike token to option contract.
+        IERC20(IPrime(_tokenP).tokenS()).transfer(_tokenP, inTokenS);
+
+        // Transfer prime token to option contract.
+        IERC20(_tokenP).transferFrom(msg.sender, _tokenP, inTokenP);
+        
+        // Call the exercise function to receive underlying tokens.
+        (, outTokenU) = IPrime(_tokenP).exercise(receiver, inTokenP, new bytes(0));
+    }
+
+    function _flash(address receiver, uint outTokenU, bytes calldata data)
+        internal
+        returns (bool)
+    {
+        // Call the exercise function to receive underlying tokens.
+        IPrime(tokenP).exercise(receiver, outTokenU, data);
+        return true;
+    }
+
+    function _redeem(address receiver, uint outTokenR) internal returns (uint inTokenS) {
+        address _tokenP = tokenP;
+        // Push tokenR to _tokenP so we can call redeem() and pull tokenS.
+        IERC20(IPrime(_tokenP).tokenR()).transfer(_tokenP, outTokenR);
+        // Call redeem function to pull tokenS.
+        inTokenS = IPrime(_tokenP).redeem(receiver);
+    }
+
+    function _close(uint inTokenR, uint inTokenP) internal returns (uint outTokenU) {
+        address _tokenP = tokenP;
+        // Transfer redeem to the option contract.
+        IERC20(IPrime(_tokenP).tokenR()).transfer(_tokenP, inTokenR);
+
+        // Transfer prime token to prime contract.
+        IERC20(_tokenP).transferFrom(msg.sender, _tokenP, inTokenP);
+        
+        // Call the close function to have the receive underlying tokens.
+        outTokenU = IPrime(_tokenP).close(address(this));
     }
 
     function balances() public override view returns (uint balanceU, uint balanceR) {
