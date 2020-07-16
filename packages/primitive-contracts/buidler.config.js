1ae51eb296fc13333756573a8320df7e039c5120
Alexangelj <alexangel.j@gmail.com>
2020-07-16 12:07:50 -0700

    Fixes Option.sol and Primitives.sol prettier formatting.

--- packages/primitive-contracts/buidler.config.js
+++ packages/primitive-contracts/buidler.config.js
@@ -23,14 +23,9 @@ usePlugin("solidity-coverage");
 unhook();
 
 // == Environment ==
-const ETHERSCAN_APY_KEY =
-    process.env.ETHERSCAN_APY_KEY || crypto.randomBytes(20).toString("base64");
-const rinkeby =
-    process.env.RINKEBY ||
-    new ethers.providers.InfuraProvider("rinkeby").connection.url;
-const mainnet =
-    process.env.MAINNET ||
-    new ethers.providers.InfuraProvider("mainnet").connection.url;
+const ETHERSCAN_APY_KEY = process.env.ETHERSCAN_APY_KEY || crypto.randomBytes(20).toString("base64");
+const rinkeby = process.env.RINKEBY || new ethers.providers.InfuraProvider("rinkeby").connection.url;
+const mainnet = process.env.MAINNET || new ethers.providers.InfuraProvider("mainnet").connection.url;
 const mnemonic = process.env.TEST_MNEMONIC || bip39.generateMnemonic();
 const live = process.env.MNEMONIC || mnemonic;
 
@@ -93,9 +88,5 @@ Object.assign(module.exports, {
         artifacts: path.join(__dirname, "artifacts"),
         deploy: path.join(__dirname, "deploy"),
         deployments: path.join(__dirname, "deployments"),
-    },
-    spdxLicenseIdentifier: {
-        overwrite: true,
-        runOnCompile: true,
     },
 });
